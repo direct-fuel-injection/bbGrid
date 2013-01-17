d595b104324f6b50c0811e6d86ad1807648fd5d6
Алексей <mr.minin@rambler.ru>
2013-01-17 20:14:02 +0300

    examples updated

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,67 +1,112 @@
-$(function(){  
-    var models;
-    var companies = new Backbone.Collection;
-    var MyCollection = new Backbone.Collection;
-    var MyGrid = new bbGrid.View({
-        container: $('#bbGrid-example'),
-//        width: 700,
-        rows: 25,
-        rowList: [25,50, 100, 250, 500],
-        collection: MyCollection,
+$(function(){
+    var App = {};
+    
+    App._exampleCollection = Backbone.Collection.extend({
+        url: 'examples.json'
+    });
+    
+    App.companies = new Backbone.Collection;
+    App.clearGridCollection = new Backbone.Collection;
+    App.exampleCollection = new App._exampleCollection();
+    // filters example
+    App.FiltersExampleGrid = new bbGrid.View({
+        container: $('#bbGrid-filters'),            
+        rows: 5,
+        rowList: [5, 25, 50, 100],
+        collection: App.exampleCollection,
         colModel: [{ title: 'ID', name: 'id', index: true, sorttype: 'number' },
                    { title: 'Full Name', index: true, name: 'name', filter: true, filterType: 'input'},
                    { title: 'Company', index: true, name: 'company', filter: true },
                    { title: 'Email', index: true, name: 'email' }
-        ],
-        buttons: [{
-                title: 'Show selected',
-                onClick: function(){
-                    var models = MyGrid.getSelectedModels()
-                    if( models.length > 0 && models[0])
-                        alert(models[0].get('name'));
-                    else
-                        alert('Nothing');
-                    
-                }
-        }]
+        ]
     });
-    var MyGrid2 = new bbGrid.View({
-        container: $('#bbGrid-example2'),
-        width: 700,
-        rows: 25,
-        rowList: [25,50, 100, 250, 500],
-        collection: companies,
-        subgrid: true,
-        subgridAccordion: true,
-        colModel: [ { title: 'Company',  index: true, name: 'company' } ],
-        onRowExpanded: function($el, rowid) {                
-            var subgridCollection = new Backbone.Collection(MyCollection.where({'company' : companies.at(rowid).get('company')}));
-            var subgrid = new bbGrid.View({
-                container: $el,
-                rows: 10,
-                multiselect: true,
-                collection: subgridCollection,                    
-                colModel: [ { title: 'Full Name', index: true,  name: 'name' },
-                   { title: 'Age', name: 'age',  index: true, sorttype: 'number' },
-                   { title: 'Address',  index: true, name: 'address' },
-                   { title: 'Email',  index: true, name: 'email' }
-                ]
+    
+    App.SearchExampleGrid = new bbGrid.View({
+        container: $('#bbGrid-search'),            
+        rows: 5,
+        rowList: [5, 25, 50, 100],
+        collection: App.exampleCollection,
+        colModel: [{ title: 'ID', name: 'id', index: true, sorttype: 'number' },
+                   { title: 'Full Name', index: true, name: 'name'},
+                   { title: 'Company', index: true, name: 'company'},
+                   { title: 'Email', index: true, name: 'email' }],
+        enableSearch: true,
+        onReady: function(){
+            $('a', this.$el).removeAttr('href');
+        }
+    });    
+    
+    App.exampleCollection.fetch({ wait: true, 
+        success:function(collection) {
+            
+            App.companies.add(_.map(_.uniq(collection.pluck('company')), function(val, index){
+                return {
+                    'id': index, 
+                    'company': val
+                };
+            }));
+            App.clearGridCollection.add(collection.models.slice(0, 10));
+            
+            App.ClearExampleGrid = new bbGrid.View({
+                container: $('#bbGrid-clear'),
+                collection: App.clearGridCollection,
+                colModel: [{ title: 'ID', name: 'id' },
+                           { title: 'Full Name', name: 'name'},
+                           { title: 'Company', name: 'company'},
+                           { title: 'Email', name: 'email' }]
+            });
+            App.ButtonsExampleGrid = new bbGrid.View({
+                container: $('#bbGrid-buttons'),
+                collection: App.clearGridCollection,
+                colModel: [{ title: 'ID', name: 'id' },
+                           { title: 'Full Name', name: 'name'},
+                           { title: 'Company', name: 'company'},
+                           { title: 'Email', name: 'email' }],
+                buttons: [{
+                        title: 'Show selected',
+                        onClick: function(){
+                            var models = this.view.getSelectedModels();
+                            if( !_.isEmpty(models))
+                                alert(_.first(models).get('name'));
+                            else
+                                alert('Nothing');
+
+                        }
+                }]
+            });
+            
+            App.SubgridExapmleGrid = new bbGrid.View({
+                container: $('#bbGrid-subgrid'),
+                rows: 5,
+                rowList: [25,50, 100],
+                collection: App.companies,
+                subgrid: true,
+                subgridAccordion: true,
+                colModel: [ { title: 'Company',  index: true, name: 'company' } ],
+                onRowExpanded: function($el, rowid) {
+                    var subgridCollection = new Backbone.Collection(collection.where({'company' : App.companies.at(rowid).get('company')}));
+                    var subgrid = new bbGrid.View({
+                        container: $el,
+                        rows: 10,
+//                        multiselect: true,
+                        collection: subgridCollection,
+                        colModel: [ { title: 'Full Name', index: true,  name: 'name' },
+                           { title: 'Age', name: 'age',  index: true, sorttype: 'number' },
+                           { title: 'Address',  index: true, name: 'address' },
+                           { title: 'Email',  index: true, name: 'email' }
+                        ]
+                    });
+                }
             });
         }
     });
-    MyGrid.toggleLoading(true);
-    MyGrid2.toggleLoading(true);
-    $.getJSON("example.json", function(json) {
-        models = json.result;
-        MyCollection.add(models, {silent: true});
-        MyCollection.trigger('reset');
-        companies.add(_.map(_.uniq(_.pluck(models, 'company')), function(val, index){
-            return {
-                'id': index, 
-                'company': val
-            };
-        }), {silent: true} );
-        companies.trigger('reset');
-    });
+    
+//    
+//    $.getJSON("examples.json", function(json) {
+//        models = json.result;
+//        MyCollection.add(models, {silent: true});
+//        MyCollection.trigger('reset');
+//        companies.trigger('reset');
+//    });
     
 });
